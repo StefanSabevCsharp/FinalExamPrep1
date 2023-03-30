f007cda43fc90ded83487df8dd625d505ed9a009
Stefan Sabev <stevenbg91@gmail.com>
2023-03-30 21:45:44 +0200

    Initial commit

---
+++ FinalExamPrep1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FinalExamPrep1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string pattern = @"(#|\|)(?<product>[A-Z][a-z]+)\1(?<date>\d{2}/\d{2}/\d{2})\1(?<callories>\d+)\1";
+            string pattern = @"(#|\|)(?<product>[A-Z][a-z]+(\s[A-Z][a-z]+)*?)\1(?<date>\d{2}/\d{2}/\d{2})\1(?<callories>\d+)\1";
+            string input = Console.ReadLine();
+            MatchCollection matchCollection = Regex.Matches(input, pattern);
+            int totalCallories = 0;
+            
+            foreach (Match match in matchCollection)
+            {
+                int currentCallories = int.Parse(match.Groups["callories"].Value);
+                totalCallories += currentCallories;
+                
+                
+            }
+            int daysAlive = totalCallories / 2000;
+            Console.WriteLine($"You have food to last you for: {daysAlive} days!");
+            foreach (Match match in matchCollection)
+            {
+                Console.WriteLine($"Item: {match.Groups["product"].Value}, Best before: {match.Groups["date"].Value}, Nutrition: {match.Groups["callories"].Value}");
+            }
+        }
+    }
+}
---
+++ Problem 3 - Plant Discovery/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Problem_3___Plant_Discovery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, Plants> plants = new Dictionary<string, Plants>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] info = Console.ReadLine().Split("<->");
+                string plantName = info[0];
+                int rarity = int.Parse(info[1]);
+                Plants plant = new Plants(plantName, rarity, new List<int>());
+                plants.Add(plantName, plant);
+            }
+            string command = Console.ReadLine();
+
+            while (command != "Exhibition")
+            {
+                string[] info = command.Split(new char[] { ' ', ':' }, StringSplitOptions.RemoveEmptyEntries);
+                string action = info[0];
+                string plantName = info[1];
+                if (action == "Rate")
+                {
+                    if (plants.ContainsKey(plantName))
+                    {
+                        int rating = int.Parse(info[3]);
+                        plants[plantName].Ratings.Add(rating);
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                }
+                else if (action == "Update")
+                {
+                    if (plants.ContainsKey(plantName))
+                    {
+                    int newRarity = int.Parse(info[3]);
+                    plants[plantName].Rarity = newRarity;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                }
+                else if (action == "Reset")
+                {
+                    if (plants.ContainsKey(plantName))
+                    {
+                        plants[plantName].Ratings.RemoveAll(x => x >= 0);
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                }
+
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine("Plants for the exhibition:");
+            foreach (var item in plants)
+            {
+                if (item.Value.Ratings.Any())
+                {
+                Console.WriteLine($"- {item.Value.Name}; Rarity: {item.Value.Rarity}; Rating: {item.Value.Ratings.Average():f2}");
+
+                }
+                else
+                {
+                    Console.WriteLine($"- {item.Value.Name}; Rarity: {item.Value.Rarity}; Rating: {0:f2}");
+                }
+               
+            }
+        }
+    }
+    class Plants
+    {
+        public string Name { get; set; }
+        public int Rarity { get; set; }
+        public List<int> Ratings { get; set; }
+
+        public Plants(string name, int rarity, List<int> ratings)
+        {
+            Name = name;
+            Rarity = rarity;
+            Ratings = ratings;
+        }
+
+    }
+}
